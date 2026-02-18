bc49f4a768696d843863c88e758eddc8fcbc6bd8
Valero-123 <era_ich@mail.ru>
2026-02-18 14:52:57 +0300

    Add files via upload

--- routes/offerRoutes.js
+++ routes/offerRoutes.js
@@ -1,8 +1,14 @@
-import {Router} from 'express';
-import { getAllOffers } from '../controllers/offerController.js';
+import { Router } from 'express';
+import upload from '../middleware/upload.js'
+import { createOffer, getAllOffers,getFullOffer } from '../controllers/offerController.js';
 
 const router = new Router();
 
 router.get('/offers', getAllOffers);
+router.post('/offers', upload.fields([
+    { name: 'previewImage', maxCount: 1 },
+    { name: 'photos', maxCount: 6 }
+]), createOffer);
+router.get('/offers/:id', getFullOffer);
 
 export default router;
